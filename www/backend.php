b80b94a9dd0a792d024f25412bfe0ecd40b04666
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2018-12-15 23:28:41 +0100

    streamlined data. added peak plots

--- www/backend.php
+++ www/backend.php
@@ -10,14 +10,30 @@ header("content-type: application/json");
 
 define("DEFAULT_INTERVAL", 7200);
 define("MAX_LEVEL", 70);
+
+$jsonaction = filter_input(INPUT_GET, "jsonaction", FILTER_SANITIZE_STRING, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$interval = filter_input(INPUT_GET, "interval", FILTER_VALIDATE_INT, array("options" => array("default" => DEFAULT_INTERVAL, "min_range" => 600))); // 10 minute minimum
+$normalized = filter_var(filter_input(INPUT_GET, "normalized", FILTER_SANITIZE_STRING, array("options" => array("default" => "true"))), FILTER_VALIDATE_BOOLEAN);
+$graphable = filter_var(filter_input(INPUT_GET, "graphable", FILTER_SANITIZE_STRING, array("options" => array("default" => "false"))), FILTER_VALIDATE_BOOLEAN);
+
+//labels based on graphable, xy = for chartjs
+$valueLabel = "Value";
+$timeLabel = "Time";
+if ($graphable) {
+    $valueLabel = "y";
+    $timeLabel = "x";
+}
+
 //DB connection
 $db = new PDO("mysql:host:trausti.local.stumph.dk;dbname=sensorpi", "sensorread", "very1secret2password3", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 function readDB($default_interval = DEFAULT_INTERVAL) {
     global $db;
+    global $valueLabel;
+    global $timeLabel;
 
     $data = array();
-    $qry = "SELECT (" . MAX_LEVEL . " - ROUND(AVG(sensorValue),0)) AS Value, Time FROM sensorpi.rawData GROUP BY UNIX_TIMESTAMP(time) DIV $default_interval ORDER BY time ASC";
+    $qry = "SELECT (" . MAX_LEVEL . " - ROUND(AVG(sensorValue),0)) AS $valueLabel, Time as $timeLabel FROM sensorpi.rawData GROUP BY UNIX_TIMESTAMP(time) DIV $default_interval ORDER BY time ASC";
     $res = $db->query($qry, PDO::FETCH_ASSOC);
     foreach ($res as $row) {
         $data[] = $row;
@@ -27,20 +43,18 @@ function readDB($default_interval = DEFAULT_INTERVAL) {
 
 /**
  * inspiration https://stackoverflow.com/questions/43851106/find-peaks-in-a-series
+ * TODO: consider https://stackoverflow.com/questions/22583391/peak-signal-detection-in-realtime-timeseries-data/22640362#22640362
  * @return array
  */
-function getRefills() {
+function getRefills($data) {
+    global $valueLabel;
+    global $timeLabel;
     $refillData = array();
-    $rawData = readDB();
-    $data = normalizeData($rawData);
-
 
     for ($i = 1; $i < count($data) - 1; $i++) {
-        $delta_prev = abs($data[$i]["Value"] - $data[$i - 1]["Value"]);
-        $delta_next = abs($data[$i]["Value"] - $data[$i + 1]["Value"]);
-
-        if ($data[$i]["Value"] > $data[$i + 1]["Value"] && $data[$i]["Value"] > $data[$i - 1]["Value"]
-        //&& $data[$i]["Value"] > 55
+        //$delta_prev = abs($data[$i][$valueLabel] - $data[$i - 1][$valueLabel]);
+        //$delta_next = abs($data[$i][$valueLabel] - $data[$i + 1][$valueLabel]);
+        if ($data[$i][$valueLabel] >= $data[$i + 1][$valueLabel] && $data[$i][$valueLabel] >= $data[$i - 1][$valueLabel] && $data[$i][$valueLabel] > 55
         ) {
             array_push($refillData, $data[$i]);
         }
@@ -54,36 +68,39 @@ function getRefills() {
  * @return array
  */
 function normalizeData($data) {
+    global $valueLabel;
+    global $timeLabel;
     $normalizedData = array();
     $max = 0;
     $min = 100;
 
     //Find max & min
     foreach ($data as $value) {
-        if ($value["Value"] > $max) {
-            $max = $value["Value"];
+        if ($value[$valueLabel] > $max) {
+            $max = $value[$valueLabel];
         }
-        if ($value["Value"] < $min) {
-            $min = $value["Value"];
+        if ($value[$valueLabel] < $min) {
+            $min = $value[$valueLabel];
         }
     }
     foreach ($data as $entry) {
-        $normalizedData[] = array("Value" => round(($entry["Value"] - $min) / ($max - $min) * 100), "Time" => $entry["Time"]);
+        $normalizedData[] = array($valueLabel => round(($entry[$valueLabel] - $min) / ($max - $min) * 100), $timeLabel => $entry[$timeLabel]);
     }
 
     return $normalizedData;
 }
 
-/**
- * TODO: implement smoothing function? https://www.allaboutcircuits.com/technical-articles/finding-maximums-in-noisy-data/
- */
+
+
 function get_current_level() {
     global $db;
+    global $valueLabel;
+    global $timeLabel;
 
     $qry = "SELECT sensorvalue AS Value, time AS Time FROM sensorpi.rawData ORDER BY time DESC LIMIT 0,1";
     $res = $db->query($qry, PDO::FETCH_ASSOC);
     foreach ($res as $row) {
-        echo "Current level: [" . $row["Value"] . "] @ " . $row["Time"];
+        echo "Current level: [" . $row[$valueLabel] . "] @ " . $row[$timeLabel];
     }
 }
 
@@ -103,24 +120,18 @@ function get_current_level() {
 //    }
 //}
 
-$jsonaction = filter_input(INPUT_GET, "jsonaction", FILTER_SANITIZE_STRING, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
 switch ($jsonaction) {
     case "pilledata":
-        $interval = filter_input(INPUT_GET, "interval", FILTER_VALIDATE_INT, array("options" => array("default" => DEFAULT_INTERVAL, "min_range" => 600))); // 10 minute minimum
         $data = normalizeData(readDB($interval));
-        echo json_encode(array("data" => $data, "refills" => getRefills()));
+        echo json_encode(array("data" => $data, "refills" => getRefills($data)));
         exit(0);
     case "piller":
-        $interval = filter_input(INPUT_GET, "interval", FILTER_VALIDATE_INT, array("options" => array("default" => DEFAULT_INTERVAL, "min_range" => 600))); // 10 minute minimum
-        $normalized = filter_var(filter_input(INPUT_GET, "normalized", FILTER_SANITIZE_STRING, array("options" => array("default" => "true"))), FILTER_VALIDATE_BOOLEAN);
         $data = readDB($interval);
         if ($normalized) {
             $data = normalizeData($data);
         }
         echo json_encode($data);
-        exit(0);
-    case "refills":
-        echo json_encode(getRefills());
         exit(0);
     default:
         echo json_encode(array("error_message" => "Invalid action or no action requested"));
--- www/js/indexchart.js
+++ www/js/indexchart.js
@@ -1,179 +1,180 @@
 function selectItemByValue(elmnt, value) {
-      for (var i = 0; i < elmnt.options.length; i++) {
+    for (var i = 0; i < elmnt.options.length; i++) {
         if (elmnt.options[i].value === value) {
-          elmnt.selectedIndex = i;
-          break;
+            elmnt.selectedIndex = i;
+            break;
         }
-      }
     }
+}
 
-    var data = [];
-    var labels = [];
-    var pilleChart;
-    var interval = 7200;
-    //$("#interval").val(interval);
-    var warningLevel = 10;
-    var emptyLevel = 4;
-    window.chartColors = {
-      red: 'rgb(255,  99, 132)',
-      orange: 'rgb(255, 159,  64)',
-      yellow: 'rgb(255, 205,  86)',
-      green: 'rgb( 75, 192, 192)',
-      blue: 'rgb( 54, 162, 235)',
-      purple: 'rgb(153, 102, 255)',
-      grey: 'rgb(231, 233, 237)',
-      black: 'rgb(  0,   0,   0)',
-      graphite: 'rgb( 50,  50,  50)'
-    };
 
-    function splitData(jsonData) {
-      // Split timestamp and data into separate arrays
-      labels = [],
-      data = [];
-      jsonData.forEach(function (packet) {
-        labels.push(new Date(packet.Time));
-        data.push(parseFloat(packet.Value));
-      });
-    }
+var pilleChart;
+var urlPilleData = 'backend.php?jsonaction=pilledata&graphable=true';
+var interval = 7200;
+//$("#interval").val(interval);
+var warningLevel = 10;
+var emptyLevel = 4;
+window.chartColors = {
+    red: 'rgb(255,  99, 132)',
+    orange: 'rgb(255, 159,  64)',
+    yellow: 'rgb(255, 205,  86)',
+    green: 'rgb( 75, 192, 192)',
+    blue: 'rgb( 54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(231, 233, 237)',
+    black: 'rgb(  0,   0,   0)',
+    graphite: 'rgb( 50,  50,  50)'
+};
 
-    function updateChartData() {
+function updateChartData() {
+    console.log("updating");
 //      var intval = document.getElementById("interval").value;
 //      interval = intval;
 //      }if (interval !== intval) {
 //        console.log("Updating interval from " + interval + " to " + intval);
 //        interval = intval;
 //      }
-      var jsonData = $.ajax({
-          url: 'backend.php?jsonaction=piller&interval=' + interval,
-          dataType: 'json'
-        }).done(function (newData) {
-          splitData(newData);
-          pilleChart.data.datasets[0].data = data;
-          pilleChart.config.data.labels = labels;
-          pilleChart.update();
-        });
+    var jsonData = $.ajax({
+        url: urlPilleData,
+        dataType: 'json'
+    }).done(function (newData) {
+        pilleChart.data.datasets[0].data = newData.data;
+        pilleChart.data.datasets[1].data = newData.refills;
+        pilleChart.config.data.labels = newData.labels;
+        pilleChart.update();
+    });
 
-    }
+}
 
-    function drawLineChart() {
+function drawLineChart() {
 
-      var jsonData = $.ajax({
-          url: 'backend.php?jsonaction=piller&interval=' + interval,
-          dataType: 'json'
-        }).done(function (rawData) {
-          splitData(rawData);
-
-          // Create the chart.js data structure using 'labels' and 'data'
-          var pilleData = {
-            labels: labels,
+    var jsonData = $.ajax({
+        url: urlPilleData,
+        dataType: 'json'
+    }).done(function (rawData) {
+        // Create the chart.js data structure using 'labels' and 'data'
+        var pilleData = {
+            //labels: data.labels,
             datasets: [{
-                fillColor: "rgba(151,187,205,0.2)",
-                strokeColor: window.chartColors.blue,
-                borderColor: window.chartColors.graphite,
-                radius: 2,
-                borderWidth: 1,
-                fill: false,
-                label: 'Pilleniveau',
-                data: data
+                    fillColor: window.chartColors.graphite,
+                    strokeColor: window.chartColors.grey,
+                    borderColor: window.chartColors.graphite,
+                    radius: 2,
+                    borderWidth: 1,
+                    fill: false,
+                    label: 'Pilleniveau',
+                    data: rawData.data
 
-              }
+                },
+                {
+                    fillColor: window.chartColors.red,
+                    strokeColor: window.chartColors.green,
+                    borderColor: window.chartColors.red,
+                    radius: 5,
+                    borderWidth: 2,
+                    showLine: false,
+                    fill: false,
+                    label: 'Refills',
+                    data: rawData.refills
+
+                }
             ]
-          };
-          var options = {
+        };
+        var options = {
             showLines: true,
             responsive: true,
             title: {
-              display: true,
-              text: 'Pilleniveau i beholder'
+                display: true,
+                text: 'Pilleniveau i beholder'
             },
             legend: {
-              display: false
+                display: false
             },
             tooltips: {
-              mode: 'index',
-              intersect: true
+                mode: 'index',
+                intersect: true
             },
             annotation: {
-              annotations: [
-                {
-                  type: 'line',
-                  mode: 'horizontal',
-                  scaleID: 'y-axis-0',
-                  value: warningLevel,
-                  borderColor: window.chartColors.orange,
-                  borderWidth: 1.5,
-                  label: {
-                    enabled: true,
-                    content: 'Refill',
-                    backgroundColor: window.chartColors.orange
-                  }
-                },
-                {
-                  type: 'line',
-                  mode: 'horizontal',
-                  scaleID: 'y-axis-0',
-                  value: emptyLevel,
-                  borderColor: window.chartColors.red,
-                  borderWidth: 1.5,
-                  label: {
-                    enabled: true,
-                    content: 'Empty',
-		    backgroundColor: window.chartColors.red
-                  }
-                }
-              ]
+                annotations: [
+                    {
+                        type: 'line',
+                        mode: 'horizontal',
+                        scaleID: 'y-axis-0',
+                        value: warningLevel,
+                        borderColor: window.chartColors.orange,
+                        borderWidth: 1.5,
+                        label: {
+                            enabled: true,
+                            content: 'Refill',
+                            backgroundColor: window.chartColors.orange
+                        }
+                    },
+                    {
+                        type: 'line',
+                        mode: 'horizontal',
+                        scaleID: 'y-axis-0',
+                        value: emptyLevel,
+                        borderColor: window.chartColors.red,
+                        borderWidth: 1.5,
+                        label: {
+                            enabled: true,
+                            content: 'Empty',
+                            backgroundColor: window.chartColors.red
+                        }
+                    }
+                ]
             },
             zoom: {
-              // Boolean to enable zooming
-              enabled: true,
+                // Boolean to enable zooming
+                enabled: true,
 
-              // Enable drag-to-zoom behavior
-              drag: true,
+                // Enable drag-to-zoom behavior
+                drag: true,
 
-              // Zooming directions. Remove the appropriate direction to disable
-              // Eg. 'y' would only allow zooming in the y direction
-              mode: 'xy',
-              rangeMin: {
-                // Format of min zoom range depends on scale type
-                x: null,
-                y: null
-              },
-              rangeMax: {
-                // Format of max zoom range depends on scale type
-                x: null,
-                y: null
-              },
-              // Function called once zooming is completed
-              // Useful for dynamic data loading
-              onZoom: /*function () {
-                console.log('I was zoomed!!!');
-              }*/ null
+                // Zooming directions. Remove the appropriate direction to disable
+                // Eg. 'y' would only allow zooming in the y direction
+                mode: 'xy',
+                rangeMin: {
+                    // Format of min zoom range depends on scale type
+                    x: null,
+                    y: null
+                },
+                rangeMax: {
+                    // Format of max zoom range depends on scale type
+                    x: 0,
+                    y: 100
+                },
+                // Function called once zooming is completed
+                // Useful for dynamic data loading
+                onZoom: function () {
+                 console.log('I was zoomed!!!');
+                 } 
             },
             scales: {
-              yAxes: [{
-                  ticks: {
-                    beginAtZero: true
-                  }
-                }
-              ],
-              //Set X-Axis to date instead of labels
-              xAxes: [{
-                  type: 'time',
-                  time: {
-                    unit: 'day'
-                  }
-                }
-              ]
+                yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }
+                ],
+                //Set X-Axis to date instead of labels
+                xAxes: [{
+                        type: 'time',
+                        time: {
+                            unit: 'day'
+                        }
+                    }
+                ]
             }
-          };
-          var ctx = document.getElementById("pilleChart").getContext("2d");
-          // Instantiate a new chart
-          pilleChart = new Chart(ctx, {
-              type: 'line',
-              data: pilleData,
-              options: options
-            });
+        };
+        var ctx = document.getElementById("pilleChart").getContext("2d");
+        // Instantiate a new chart
+        pilleChart = new Chart(ctx, {
+            type: 'line',
+            data: pilleData,
+            options: options
         });
-    }
+    });
+}
 
-    drawLineChart();
+drawLineChart();
